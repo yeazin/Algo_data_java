5463234937ec73f1e4d60e838fb0d1bfe61292c0
Md. N. Islam Yeasin <naz.yeasin@gmail.com>
2023-03-24 19:54:14 +0600

    Update FindCustomerIntervals.java

--- FindCustomerIntervals.java
+++ FindCustomerIntervals.java
@@ -1,3 +1,28 @@
-// Find customer in java 
+// find customer in an interval
+// time complexity O(n)
+// space complexity O(1)
 
-import java.util.*;
+
+// Find total customer in a Time interval 
+
+public class FindCustomerInterval {
+
+    public static void main(String[] args) {
+
+        int arrival_time[] = { 10, 12, 14, 15, 18, 20, 22 };
+        int interval[] = { 12, 18 };
+        int count_customer = 0;
+
+        for (int i = 0; i < arrival_time.length; i++) {
+
+            if (arrival_time[i] >= interval[0] &&
+                    arrival_time[i] <= interval[1]) {
+
+                count_customer++;
+            }
+        }
+
+        System.out.println("Total Customer" + " " + count_customer);
+
+    }
+}
